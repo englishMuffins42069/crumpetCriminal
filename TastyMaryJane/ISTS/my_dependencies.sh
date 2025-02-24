81b9aa7c4eecbdf916f465785c196d922e6b0644
englishMuffins42069 <ARKing04.03@gmail.com>
2025-02-24 12:24:15 -0600

    Delete TastyMaryJane/ISTS directory

--- TastyMaryJane/ISTS/my_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Updating package lists..."
-sudo apt update -y
-
-echo "Installing required packages..."
-sudo apt install -y sshpass telnet netcat iptables
-
-echo "All necessary packages installed successfully!"
